6b1ce856a86240dcca5587609e3b05267e5e5320
Sean Semyck <seanpsemyck98@gmail.com>
2025-12-28 16:11:04 -0600

    Header component initialized

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "../Header/Header.jsx";
 
 function App() {
-  return <div className="app">Test</div>;
+  return (
+    <div className="app">
+      <div className="app__content">
+        Test
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
