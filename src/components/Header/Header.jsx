c3209dcd6245ef6dbc9c6ea922447b0a297f9bcf
Sean Semyck <seanpsemyck98@gmail.com>
2025-12-28 22:10:53 -0600

    Header images inserted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
+import "./Header.css";
+import appLogo from "../../assets/logo.svg";
+import userAvatar from "../../assets/avatar-image.png";
+
 function Header() {
   return (
     <Header className="header">
-      <img className="header__logo" />
+      <img src={appLogo} alt="App Logo" className="header__logo" />
       <p className="header__date-and-location">DATE, LOCATION</p>
       <button className="header__add-cloth-btn">+ Add clothes</button>
       <div className="header__user">
         <p className="header__user_name">NAME</p>
-        <img className="header__user_avatar" />
+        <img
+          src={userAvatar}
+          alt="User Avatar"
+          className="header__user_avatar"
+        />
       </div>
     </Header>
   );
