b6db479ce616b53ba125d1a1074f0aa96307beae
Sean Semyck <seanpsemyck98@gmail.com>
2025-12-30 18:11:25 -0600

    Footer added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header.jsx";
 import Main from "../Main/Main.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
       </div>
+      <Footer />
     </div>
   );
 }
