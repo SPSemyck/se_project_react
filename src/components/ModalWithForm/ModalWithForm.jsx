96889191c1921516d5afc5ef14ef29856b7cb9bc
Sean Semyck <seanpsemyck98@gmail.com>
2025-12-31 16:35:47 -0600

    handleCloseClick for modal close

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,13 +1,23 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, title, buttonText, activeModal }) {
+function ModalWithForm({
+  children,
+  title,
+  buttonText,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
     <div
       className={`modal ${activeModal === "add-garment" && "modal_is-open"}`}
     >
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button type="button" className="modal__close-btn">
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close-btn"
+        >
           Close
         </button>
         <form className="modal__form">
